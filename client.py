bbfd76834628343c4c7c37d5ba91ec567cfb5b1f
George du Plooy <DPLGEO001@myuct.ac.za>
2022-03-07 12:17:04 +0200

    message fixed

--- client.py
+++ client.py
@@ -3,7 +3,7 @@ serverName = 'hostname'
 serverPort = 12000
 clientSocket = socket(AF_INET, SOCK_DGRAM)
 
-message = raw_input('Input sentence:')
+message = input('Input sentence:')
 clientSocket.sendto(message.encode(),(serverName, serverPort))
 modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
 print (modifiedMessage.decode())
